c8c10f680e9052becfda503f2e8484ebb92f19a6
siddharth <sedflix@gmail.com>
2021-06-05 17:33:24 +0530

    Add installation of istio multi-cluster using helm (#1)
    * add chart version1
    
    * fix blunders of life
    
    * fix blunder: label namespace too
    
    * refactor folder structure
    
    * refactor stuff back
    
    * updae readme and make scripts

--- kind-setup/install-cacerts.sh
+++ kind-setup/install-cacerts.sh
@@ -15,6 +15,8 @@ make -f ../tools/certs/Makefile.selfsigned.mk root-ca
 for i in $(seq "${NUM_CLUSTERS}"); do
   make -f ../tools/certs/Makefile.selfsigned.mk "cluster${i}-cacerts"
   kubectl create namespace istio-system --context "cluster${i}"
+  kubectl --context="cluster${i}" label namespace istio-system topology.istio.io/network="network${i}"
+  kubectl delete secret cacerts -n istio-system --context "cluster${i}"
   kubectl create secret generic cacerts -n istio-system --context "cluster${i}" \
       --from-file="cluster${i}/ca-cert.pem" \
       --from-file="cluster${i}/ca-key.pem" \
