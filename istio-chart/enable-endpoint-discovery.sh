fea1840372233b11c29b96d957594508b28585bf
S. M. Mahmudul Haque <haque@campus.tu-berlin.de>
2025-02-08 04:22:06 +0100

    Fix broken command

--- istio-chart/enable-endpoint-discovery.sh
+++ istio-chart/enable-endpoint-discovery.sh
@@ -5,25 +5,21 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-
-
 OS="$(uname)"
 NUM_CLUSTERS="${NUM_CLUSTERS:-2}"
 
 for i in $(seq "${NUM_CLUSTERS}"); do
   for j in $(seq "${NUM_CLUSTERS}"); do
-    if [ "$i" -ne "$j" ]
-    then
+    if [ "$i" -ne "$j" ]; then
       echo "Enable Endpoint Discovery between cluster${i} and cluster${j}"
 
-      if [ "$OS" == "Darwin" ]
-      then
+      if [ "$OS" == "Darwin" ]; then
         # Set container IP address as kube API endpoint in order for clusters to reach kube API servers in other clusters.
         docker_ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' "cluster${i}-control-plane")
-        istioctl x create-remote-secret \
-        --context="cluster${i}" \
-        --server="https://${docker_ip}:6443" \
-        --name="cluster${i}" | \
+        istioctl create-remote-secret \
+          --context="cluster${i}" \
+          --server="https://${docker_ip}:6443" \
+          --name="cluster${i}" | \
           kubectl apply --validate=false --context="cluster${j}" -f -
       else
         istioctl x create-remote-secret \
