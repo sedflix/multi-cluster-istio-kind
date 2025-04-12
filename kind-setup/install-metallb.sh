b0f45cee57be5288442f4a2525ac5e1263cb4145
Ric Hincapie <hincapiet.ricardo@gmail.com>
2025-04-12 15:43:47 +1000

    Make metallb deploy faster.

--- kind-setup/install-metallb.sh
+++ kind-setup/install-metallb.sh
@@ -13,13 +13,12 @@ NUM_CLUSTERS="${NUM_CLUSTERS:-2}"
 for i in $(seq "${NUM_CLUSTERS}"); do
   sleep_duration=$((90/i))
   echo "Starting metallb deployment in cluster${i}"
-  # kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.8/manifests/namespace.yaml --context "cluster${i}"
-  #kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.8/manifests/metallb.yaml --context "cluster${i}"
+
   kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.10/config/manifests/metallb-native.yaml --context "cluster${i}"
   kustomize build ${DIR}/kind-setup/metalLb | kubectl apply -f -
-  # kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"   --context "cluster${i}"
-  sleep ${sleep_duration}
   # TODO: this metallb-configmap need to be automatically generated
+  kubectl wait --for=condition=available --timeout=90s deployment/controller \
+      -n metallb-system --context "cluster${i}"
   kubectl apply -f ${DIR}/kind-setup/metallb-configmap-${i}-copy.yaml --context "cluster${i}"
   echo "----"
 done
